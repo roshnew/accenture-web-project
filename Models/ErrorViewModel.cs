baeea87f062bceb4838636a91488b4db3bf44ced
makarand.bhoir <makarand.bhoir@live.com>
2025-11-11 10:58:18 +0530

    my first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace accenture_web_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
